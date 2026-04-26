3f690cb4653bc035b0be89999473ff5876f33815
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2026-04-26 10:50:12 +0545

    knn

--- knn/knn.py
+++ knn/knn.py
@@ -10,7 +10,6 @@ from sklearn.metrics import accuracy_score, classification_report
 # Load the locally saved Titanic CSV
 df = pd.read_csv('/Users/sumanshrestha/Downloads/titanic.csv')
 
-# Preview the dataset
 print(df.head())
 
 # Step 3: Preprocess data
